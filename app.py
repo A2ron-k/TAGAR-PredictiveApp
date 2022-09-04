32bb29f663518c0871dee4ed9c7681c4324a2c06
a2ron-k <aaronkwah@gmail.com>
2022-09-04 22:13:57 +0800

    doc: add documentation to run

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-from distutils.command.config import config
+### Running instruction
+# 1. Create Virtual Environment => * python -m venv env
+# 2. Activate Virtual Environment => * source env/bin/activate (For Mac) | * .\env\Scripts\activate (For Windows)
+# 3. Install dependencies and packages => * pip install -r requirements.txt
+# 3. Change directory to TAGAR_PDA_REMASTER
+# 4. Cmd to config flask app to search for the initaliser location aka inside app => * export FLASK_APP=app (For Mac) | * set FLASK_APP=app (For Windows)
+# 5. Cmd to config flask app DEBUG MODE => * export FLASK_DEBUG=1 (For Mac) | * set FLASK_DEBUG=1 (For Windows)
+# 6. Run using app while in TAGAR_PDA_REMASTER directory => * flask run 
+
+# For Mac (Single line flask run) -> export FLASK_APP=app; export FLASK_ENV=development; export FLASK_DEBUG=1; flask run 
+###
+
 from flask import Flask, render_template
 from flask_session import Session
 from dotenv import load_dotenv, find_dotenv
